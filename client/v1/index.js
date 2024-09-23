8dfbcf2a8d93d7ccafd7d5654ab5141e4cf3bdb0
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2024-09-23 15:39:07 +0200

    feat(total): info about communities

--- client/v1/index.js
+++ client/v1/index.js
@@ -53,6 +53,10 @@ console.log(`number of deals: ${nb_deals}`);
 // 2. Log the variable
 // 3. Log how many shopping communities we have
 
+var unique_communities = Array.from(new Set(deals.map(deal => deal["community"])));
+console.log(unique_communities);
+console.log(`number of communities: ${unique_communities.length}`);
+
 // 🎯 TODO 4: Sort by price
 // 1. Create a function to sort the deals by price
 // 2. Create a variable and assign it the list of sets by price from lowest to highest
