20ebcb79f1e09b2253bc72c60f0bf014363b764d
Joyce <lianajoycemarie@gmail.com>
2024-11-18 19:16:59 +0100

    feat(mongodb): add sorting by price and date

--- server/node.js
+++ server/node.js
@@ -11,105 +11,131 @@ const SALES_COLLECTION = "sales";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function insertDeals(db) {
-  // Read and parse the deals.json file
-  const dealsFilePath = "data/deals.json";
-  const deals = [...JSON.parse(fs.readFileSync(dealsFilePath, "utf-8"))];
+    // Read and parse the deals.json file
+    const dealsFilePath = "data/deals.json";
+    const deals = [...JSON.parse(fs.readFileSync(dealsFilePath, "utf-8"))];
 
-  const collection = db.collection(DEALS_COLLECTION);
-  const result = await collection.insertMany(deals);
+    const collection = db.collection(DEALS_COLLECTION);
+    const result = await collection.insertMany(deals);
 
-  console.log(`${result.insertedCount} deals records inserted`);
+    console.log(`${result.insertedCount} deals records inserted`);
 }
 
 async function insertSales(db) {
-  // Path to the folder containing the JSON files
-  const salesFolderPath = "data/sales";
+    // Path to the folder containing the JSON files
+    const salesFolderPath = "data/sales";
 
-  // Read all the files in the folder
-  const salesFiles = fs.readdirSync(salesFolderPath);
+    // Read all the files in the folder
+    const salesFiles = fs.readdirSync(salesFolderPath);
 
-  const sales = [];
+    const sales = [];
 
-  // Iterate over each file, read and parse the JSON content
-  for (const file of salesFiles) {
-    const filePath = path.join(salesFolderPath, file);
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-    const salesData = JSON.parse(fileContent);
+    // Iterate over each file, read and parse the JSON content
+    for (const file of salesFiles) {
+        const filePath = path.join(salesFolderPath, file);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+        const salesData = JSON.parse(fileContent);
 
-    // Ensure the parsed content is an array and add it to sales
-    if (Array.isArray(salesData)) {
-      sales.push(...salesData);
-    } else {
-      console.warn(`File ${file} did not contain an array of sales data`);
+        // Ensure the parsed content is an array and add it to sales
+        if (Array.isArray(salesData)) {
+            sales.push(...salesData);
+        } else {
+            console.warn(`File ${file} did not contain an array of sales data`);
+        }
     }
-  }
 
-  // Insert all sales data into the "sales" collection
-  const collection = db.collection(SALES_COLLECTION);
-  const result = await collection.insertMany(sales);
+    // Insert all sales data into the "sales" collection
+    const collection = db.collection(SALES_COLLECTION);
+    const result = await collection.insertMany(sales);
 
-  console.log(`${result.insertedCount} sales records inserted`);
+    console.log(`${result.insertedCount} sales records inserted`);
 }
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    // Access the database
-    const database = client.db(MONGODB_DB_NAME);
+        // Access the database
+        const database = client.db(MONGODB_DB_NAME);
 
-    // Add data to collections
-    // await insertDeals(database);
-    // await insertSales(database);
+        // Add data to collections
+        // await insertDeals(database);
+        // await insertSales(database);
 
-    // await findBestDiscountDeals(database, 40);
-    await findMostCommentedDeals(database);
+        // await findBestDiscountDeals(database, 40);
+        // await findMostCommentedDeals(database);
+        // await findDealsSortedByPrice(database);
+        await findDealsSortedByDate(database);
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
 
 run().catch(console.dir);
 
 async function findBestDiscountDeals(db, minDiscount) {
-  const collection = db.collection(DEALS_COLLECTION);
+    const collection = db.collection(DEALS_COLLECTION);
 
-  const bestDiscountDeals = await collection
-    .find({ discount: { $gte: minDiscount } })
-    .sort({ discount: -1 })
-    .toArray();
-  console.log(`Deals with Discount above ${minDiscount}%: `);
-  return bestDiscountDeals;
+    const bestDiscountDeals = await collection
+        .find({ discount: { $gte: minDiscount } })
+        .sort({ discount: -1 })
+        .toArray();
+    console.log(`Deals with Discount above ${minDiscount}%: `);
+    return bestDiscountDeals;
 }
 
 async function findMostCommentedDeals(db, minComments = 100) {
-  const collection = db.collection(DEALS_COLLECTION);
+    const collection = db.collection(DEALS_COLLECTION);
 
-  const mostCommentedDeals = await collection
-    .find({ comments: { $gte: minComments } })
-    .sort({ comments: -1 })
-    .toArray();
-  console.log(mostCommentedDeals);
-  return mostCommentedDeals;
+    const mostCommentedDeals = await collection
+        .find({ comments: { $gte: minComments } })
+        .sort({ comments: -1 })
+        .toArray();
+    console.log(mostCommentedDeals);
+    return mostCommentedDeals;
+}
+
+async function findDealsSortedByPrice(db, ascending = false) {
+    const collection = db.collection(DEALS_COLLECTION);
+
+    const sortedDeals = await collection
+        .find()
+        .sort({ price: ascending ? 1 : -1 })
+        .toArray();
+    console.log(sortedDeals);
+    return sortedDeals;
+}
+
+async function findDealsSortedByDate(db, ascending = false) {
+    const collection = db.collection(DEALS_COLLECTION);
+
+    const sortedDeals = await collection
+        .find()
+        .sort({ publication: ascending ? 1 : -1 })
+        .toArray();
+    console.log(sortedDeals);
+    return sortedDeals;
 }
 
 module.exports = {
-  findBestDiscountDeals,
-  findMostCommentedDeals,
+    findBestDiscountDeals,
+    findMostCommentedDeals,
+    findDealsSortedByPrice,
+    findDealsSortedByDate,
 };
