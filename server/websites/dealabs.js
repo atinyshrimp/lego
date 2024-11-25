77f72009b722a33f20b47cae7cf827e5c571df19
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2024-11-25 15:45:03 +0100

    feat(mongodb): override _id field in atlas

--- server/websites/dealabs.js
+++ server/websites/dealabs.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const uuidv5 = require("uuid").v5;
 
 /** Parses the HTML response and extracts deal information.
  *
@@ -8,69 +9,69 @@ const fs = require("fs");
  * @returns {Object[]} - An array of deal objects containing details such as imgUrl, title, legoId, price, etc.
  */
 const parse = (data) => {
-  const $ = cheerio.load(data, { xmlMode: true }, true);
+	const $ = cheerio.load(data, { xmlMode: true }, true);
 
-  return $("article.thread")
-    .map((_, element) => {
-      const content = JSON.parse(
-        $(element).find("div.js-vue2").attr("data-vue2")
-      ).props.thread;
+	return $("article.thread")
+		.map((_, element) => {
+			const content = JSON.parse(
+				$(element).find("div.js-vue2").attr("data-vue2")
+			).props.thread;
 
-      /** Get content from the "data-vue2" */
-      const id = content.threadId;
-      const title = content.title;
+			/** Get content from the "data-vue2" */
+			const _id = uuidv5(content.shareableLink, uuidv5.URL);
+			const title = content.title;
 
-      /** Get the Lego set ID from the title of the deal */
-      const idPattern = /\d{5}/; // Regular expression pattern for 5 digits in a row (Lego set ID)
-      const foundLegoId = title.match(idPattern);
-      const legoId = foundLegoId === null ? "" : foundLegoId[0];
+			/** Get the Lego set ID from the title of the deal */
+			const idPattern = /\d{5}/; // Regular expression pattern for 5 digits in a row (Lego set ID)
+			const foundLegoId = title.match(idPattern);
+			const legoId = foundLegoId === null ? "" : foundLegoId[0];
 
-      /** Get the different links */
-      const link = content.shareableLink; // URL to the description of the deal on Dealabs
-      const merchantLink = content.link; // URL to the original link of the offer
+			/** Get the different links */
+			const link = content.shareableLink; // URL to the description of the deal on Dealabs
+			const merchantLink = content.link; // URL to the original link of the offer
 
-      /** Get the price infos */
-      const price = content.price;
+			/** Get the price infos */
+			const price = content.price;
 
-      const nextBestPrice = content.nextBestPrice;
+			const nextBestPrice = content.nextBestPrice;
 
-      const discount =
-        nextBestPrice !== 0
-          ? Number(Number((1 - price / nextBestPrice) * 100).toFixed(0))
-          : NaN;
+			const discount =
+				nextBestPrice !== 0
+					? Number(Number((1 - price / nextBestPrice) * 100).toFixed(0))
+					: NaN;
 
-      /** Get different dates */
-      const publication = content.publishedAt;
-      const expirationDate =
-        content.endDate !== null ? content.endDate.timestamp : null;
+			/** Get different dates */
+			const publication = content.publishedAt;
+			const expirationDate =
+				content.endDate !== null ? content.endDate.timestamp : null;
 
-      /** Miscellaneous infos */
-      const comments = content.commentCount;
+			/** Miscellaneous infos */
+			const comments = content.commentCount;
 
-      const temperature = content.temperature;
+			const temperature = content.temperature;
 
-      /** Get image URL from brother element */
-      const imgUrl = JSON.parse(
-        $(element).find("div.threadGrid-image div.js-vue2").attr("data-vue2")
-      ).props.threadImageUrl;
+			/** Get image URL from brother element */
+			const imgUrl = JSON.parse(
+				$(element).find("div.threadGrid-image div.js-vue2").attr("data-vue2")
+			).props.threadImageUrl;
 
-      return {
-        id,
-        imgUrl,
-        title,
-        legoId,
-        price,
-        nextBestPrice,
-        discount,
-        link,
-        merchantLink,
-        comments,
-        temperature,
-        publication,
-        expirationDate,
-      };
-    })
-    .get();
+			return {
+				_id,
+				imgUrl,
+				title,
+				legoId,
+				price,
+				nextBestPrice,
+				discount,
+				link,
+				merchantLink,
+				comments,
+				temperature,
+				publication,
+				expirationDate,
+			};
+		})
+		.get();
 };
 
 /** Saves a JSON document to a file.
@@ -80,20 +81,20 @@ const parse = (data) => {
  * @param {Object[]} document - The JSON document to be saved.
  */
 const saveJSONfile = (path, fileName, document) => {
-  try {
-    // Check path name
-    const pathName = String(path).endsWith("/") ? path : path + "/";
+	try {
+		// Check path name
+		const pathName = String(path).endsWith("/") ? path : path + "/";
 
-    // Store the JSON documents into a JSON file
-    fs.writeFileSync(
-      `${pathName}${fileName}.json`,
-      JSON.stringify(document, null, 2)
-    );
+		// Store the JSON documents into a JSON file
+		fs.writeFileSync(
+			`${pathName}${fileName}.json`,
+			JSON.stringify(document, null, 2)
+		);
 
-    console.log(`${document.length} documents saved!`);
-  } catch (e) {
-    console.error(e);
-  }
+		console.log(`${document.length} documents saved!`);
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 /** Scrapes deals from a given URL and saves the data as JSON.
@@ -104,53 +105,53 @@ const saveJSONfile = (path, fileName, document) => {
  * @returns {Promise<Object[]>} - A promise that resolves to an array of deal objects.
  */
 module.exports.scrape = async (
-  baseUrl = "https://www.dealabs.com/groupe/lego?hide_expired=true"
+	baseUrl = "https://www.dealabs.com/groupe/lego?hide_expired=true"
 ) => {
-  console.log(`🕵️‍♀️  browsing ${baseUrl}`);
+	console.log(`🕵️‍♀️  browsing ${baseUrl}`);
 
-  let currentPage = 1;
-  let allDeals = [];
-  let hasMorePages = true;
+	let currentPage = 1;
+	let allDeals = [];
+	let hasMorePages = true;
 
-  while (hasMorePages) {
-    // Update page
-    const url = `${baseUrl}${
-      baseUrl.includes("?") ? "&" : "?"
-    }page=${currentPage}`;
-    console.log(`Scraping page: ${currentPage}`);
+	while (hasMorePages) {
+		// Update page
+		const url = `${baseUrl}${
+			baseUrl.includes("?") ? "&" : "?"
+		}page=${currentPage}`;
+		console.log(`Scraping page: ${currentPage}`);
 
-    // Making the browser believe the scraper is a "real person"
-    const opts = {
-      method: "GET",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-      },
-    };
+		// Making the browser believe the scraper is a "real person"
+		const opts = {
+			method: "GET",
+			headers: {
+				"User-Agent":
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+			},
+		};
 
-    // Get HTTP response
-    const response = await fetch(url, opts);
+		// Get HTTP response
+		const response = await fetch(url, opts);
 
-    if (response.ok) {
-      const body = await response.text();
-      const dealsDoc = parse(body);
-      if (dealsDoc.length === 0) {
-        hasMorePages = false;
-      } else {
-        allDeals = allDeals.concat(dealsDoc);
-        currentPage++;
-      }
-    } else {
-      console.error(`Failed to fetch page ${currentPage}:`, response.status);
-      hasMorePages = false;
-    }
-  }
+		if (response.ok) {
+			const body = await response.text();
+			const dealsDoc = parse(body);
+			if (dealsDoc.length === 0) {
+				hasMorePages = false;
+			} else {
+				allDeals = allDeals.concat(dealsDoc);
+				currentPage++;
+			}
+		} else {
+			console.error(`Failed to fetch page ${currentPage}:`, response.status);
+			hasMorePages = false;
+		}
+	}
 
-  // Remove empty legoId, mostly not Lego sets
-  allDeals = allDeals.filter((deal) => deal.legoId !== "");
+	// Remove empty legoId, mostly not Lego sets
+	allDeals = allDeals.filter((deal) => deal.legoId !== "");
 
-  console.log(`Saving ${allDeals.length} JSON documents...`);
-  saveJSONfile("server/data", "deals", allDeals);
-  // Return the JSON documents
-  return allDeals;
+	console.log(`Saving ${allDeals.length} JSON documents...`);
+	saveJSONfile("data", "deals", allDeals);
+	// Return the JSON documents
+	return allDeals;
 };
--- server/websites/vinted.js
+++ server/websites/vinted.js
@@ -1,18 +1,19 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const tqdm = require("tqdm");
+const uuidv5 = require("uuid").v5;
 
 // Get cookies for Vinted session
 var cookieString;
 
 var opts = {
-    method: "GET",
-    headers: {
-        Accept: "application/json, text/plain, */*",
-        Cookie: cookieString,
-        "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-    },
+	method: "GET",
+	headers: {
+		Accept: "application/json, text/plain, */*",
+		Cookie: cookieString,
+		"User-Agent":
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+	},
 };
 
 /** Extracts cookies from the Vinted website.
@@ -21,18 +22,18 @@ var opts = {
  * @returns {Promise<string>} - A string of cookies in the format "name=value; name=value".
  */
 async function extractCookies() {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto("https://www.vinted.fr", { waitUntil: "networkidle2" });
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto("https://www.vinted.fr", { waitUntil: "networkidle2" });
 
-    // Extract cookies
-    const cookies = await page.cookies();
-    const cookieString = cookies
-        .map((cookie) => `${cookie.name}=${cookie.value}`)
-        .join("; ");
+	// Extract cookies
+	const cookies = await page.cookies();
+	const cookieString = cookies
+		.map((cookie) => `${cookie.name}=${cookie.value}`)
+		.join("; ");
 
-    await browser.close();
-    return cookieString;
+	await browser.close();
+	return cookieString;
 }
 
 /** Parses the body of the Vinted API response to extract sale details.
@@ -41,24 +42,24 @@ async function extractCookies() {
  * @returns {Object[]} - An array of sale objects containing id, price, imgUrl, title, and link.
  */
 function parse(body, legoId) {
-    return body.map((sale) => {
-        const id = sale.id;
-        const price = Number(sale.total_item_price.amount);
-        const imgUrl = sale.photo.url;
-        const title = sale.title;
-        const link = sale.url;
-        const publicationDate = sale.photo.high_resolution.timestamp;
+	return body.map((sale) => {
+		const _id = uuidv5(sale.url, uuidv5.URL);
+		const price = Number(sale.total_item_price.amount);
+		const imgUrl = sale.photo.url;
+		const title = sale.title;
+		const link = sale.url;
+		const publicationDate = sale.photo.high_resolution.timestamp;
 
-        return {
-            legoId,
-            link,
-            price,
-            title,
-            imgUrl,
-            publicationDate,
-            id,
-        };
-    });
+		return {
+			_id,
+			legoId,
+			link,
+			price,
+			title,
+			imgUrl,
+			publicationDate,
+		};
+	});
 }
 
 /** Reads and extracts unique Lego IDs from the deals.json file.
@@ -66,14 +67,14 @@ function parse(body, legoId) {
  * @returns {number[]} - An array of unique Lego IDs sorted in ascending order.
  */
 function getLegoIds() {
-    // Read and parse the deals.json file
-    const dealsFilePath = "server/data/deals.json";
-    const dealsData = JSON.parse(fs.readFileSync(dealsFilePath, "utf-8"));
+	// Read and parse the deals.json file
+	const dealsFilePath = "data/deals.json";
+	const dealsData = JSON.parse(fs.readFileSync(dealsFilePath, "utf-8"));
 
-    // Extract unique Lego IDs from the deals data
-    return [
-        ...new Set(dealsData.map((deal) => deal.legoId).sort((a, b) => a - b)),
-    ];
+	// Extract unique Lego IDs from the deals data
+	return [
+		...new Set(dealsData.map((deal) => deal.legoId).sort((a, b) => a - b)),
+	];
 }
 
 /** Scrapes Vinted for items related to a given Lego ID.
@@ -85,48 +86,48 @@ function getLegoIds() {
  * @returns {Promise<Object[]>} - A promise that resolves to an array of item objects.
  */
 async function scrapeVintedForLegoId(legoId, writable = true) {
-    let allItems = [];
-    let currentPage = 1;
-    let hasMorePages = true;
-    const unixNow = Math.floor(Date.now() / 1000);
+	let allItems = [];
+	let currentPage = 1;
+	let hasMorePages = true;
+	const unixNow = Math.floor(Date.now() / 1000);
 
-    while (hasMorePages) {
-        const url = `https://www.vinted.fr/api/v2/catalog/items?page=${currentPage}&per_page=96&time=${unixNow}&search_text=${legoId}&catalog_ids=&size_ids=&brand_ids=89162&status_ids=&material_ids=`;
+	while (hasMorePages) {
+		const url = `https://www.vinted.fr/api/v2/catalog/items?page=${currentPage}&per_page=96&time=${unixNow}&search_text=${legoId}&catalog_ids=&size_ids=&brand_ids=89162&status_ids=&material_ids=`;
 
-        try {
-            const response = await fetch(url, opts);
-            if (response.ok) {
-                const data = await response.json();
-                if (data.items.length > 0) {
-                    allItems = allItems.concat(parse(data.items, legoId));
-                    currentPage++;
-                    hasMorePages = currentPage <= data.pagination.total_pages;
-                } else {
-                    hasMorePages = false;
-                }
-            } else {
-                console.error(
-                    `Failed to fetch data for Lego ID ${legoId}:`,
-                    response.status
-                );
-                hasMorePages = false;
-            }
-        } catch (error) {
-            console.error(`Error fetching data for Lego ID ${legoId}:`, error);
-            hasMorePages = false;
-        }
-    }
+		try {
+			const response = await fetch(url, opts);
+			if (response.ok) {
+				const data = await response.json();
+				if (data.items.length > 0) {
+					allItems = allItems.concat(parse(data.items, legoId));
+					currentPage++;
+					hasMorePages = currentPage <= data.pagination.total_pages;
+				} else {
+					hasMorePages = false;
+				}
+			} else {
+				console.error(
+					`Failed to fetch data for Lego ID ${legoId}:`,
+					response.status
+				);
+				hasMorePages = false;
+			}
+		} catch (error) {
+			console.error(`Error fetching data for Lego ID ${legoId}:`, error);
+			hasMorePages = false;
+		}
+	}
 
-    if (writable) {
-        // Save the items to a separate JSON file for this Lego ID
-        const filePath = `server/data/sales/${legoId}.json`;
-        fs.writeFileSync(filePath, JSON.stringify(allItems, null, 2));
-        // console.log(
-        //     `Saved ${allItems.length} items for Lego ID ${legoId} to ${filePath}`
-        // );
-    }
+	if (writable) {
+		// Save the items to a separate JSON file for this Lego ID
+		const filePath = `data/sales/${legoId}.json`;
+		fs.writeFileSync(filePath, JSON.stringify(allItems, null, 2));
+		// console.log(
+		//     `Saved ${allItems.length} items for Lego ID ${legoId} to ${filePath}`
+		// );
+	}
 
-    return allItems;
+	return allItems;
 }
 
 /** Pauses execution for a specified amount of time.
@@ -135,7 +136,7 @@ async function scrapeVintedForLegoId(legoId, writable = true) {
  * @returns {Promise<void>} - A promise that resolves after the specified delay.
  */
 function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 /** Main function to scrape Vinted for Lego deals. Can scrape for a single Lego ID or all IDs from the deals.json file.
@@ -145,26 +146,26 @@ function sleep(ms) {
  * @returns {Promise<void>} - A promise that resolves when the scraping is complete.
  */
 module.exports.scrape = async (legoId = undefined) => {
-    cookieString = await extractCookies();
-    opts.headers.Cookie = cookieString;
+	cookieString = await extractCookies();
+	opts.headers.Cookie = cookieString;
 
-    if (legoId === undefined) {
-        const legoIds = getLegoIds();
-        let counter = 0;
+	if (legoId === undefined) {
+		const legoIds = getLegoIds();
+		let counter = 0;
 
-        for (const legoId of tqdm(legoIds)) {
-            counter++;
-            // console.log(`[${counter}/${legoIds.length}]`);
-            await scrapeVintedForLegoId(legoId);
+		for (const legoId of tqdm(legoIds)) {
+			counter++;
+			// console.log(`[${counter}/${legoIds.length}]`);
+			await scrapeVintedForLegoId(legoId);
 
-            // Delay of 1000ms (1 second) between each request
-            await sleep(1000);
-        }
+			// Delay of 1000ms (1 second) between each request
+			await sleep(1000);
+		}
 
-        console.log(`Scraping completed for all ${legoIds.length} Lego IDs.`);
-    } else {
-        const sales = await scrapeVintedForLegoId(legoId, false);
-        console.log(sales);
-        console.log(`Scraping completed for Lego ID ${legoId}.`);
-    }
+		console.log(`Scraping completed for all ${legoIds.length} Lego IDs.`);
+	} else {
+		const sales = await scrapeVintedForLegoId(legoId, false);
+		console.log(sales);
+		console.log(`Scraping completed for Lego ID ${legoId}.`);
+	}
 };
