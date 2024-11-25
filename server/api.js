ad5ee3c3835a696f03bc91e5247551f0441e8c57
Joyce <99140907+atinyshrimp@users.noreply.github.com>
2024-11-25 16:18:38 +0100

    feat(api): add endpoint to find deal by id
    - Added directory dedicated to MongoDB operations

--- server/api.js
+++ server/api.js
@@ -1,6 +1,16 @@
-const cors = require('cors');
-const express = require('express');
-const helmet = require('helmet');
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const {
+	findBestDiscountDeals,
+	findMostCommentedDeals,
+	findDealsSortedByPrice,
+	findDealsSortedByDate,
+	findSalesByLegoSetId,
+	findRecentSales,
+} = require("./mongodb/queries"); // Adjust the path as needed
+require("dotenv").config();
 
 const PORT = 8092;
 
@@ -8,14 +18,50 @@ const app = express();
 
 module.exports = app;
 
-app.use(require('body-parser').json());
+app.use(require("body-parser").json());
 app.use(cors());
 app.use(helmet());
 
-app.options('*', cors());
+app.options("*", cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+/** Set Up MongoDB Connection */
+const uri = `mongodb+srv://joycelapilus:${process.env.MONGODB_CLUSTER_PWD}@fullstack-lego.lyvzb.mongodb.net/?retryWrites=true&w=majority&appName=fullstack-lego`;
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+let db;
+async function connectToDatabase() {
+	try {
+		await client.connect();
+		db = client.db(process.env.MONGODB_DB);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error);
+	}
+}
+
+connectToDatabase();
+
+/** Endpoints */
+app.get("/", (req, res) => {
+	res.send({ ack: true });
+});
+
+// Get deals by ID
+app.get("/deals/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const deal = await db.collection("deals").find({ _id: id }).toArray();
+		res.status(200).json(deal);
+	} catch (error) {
+		console.error("Error fetching the deal: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 });
 
 app.listen(PORT);
---
+++ server/mongodb/node.js
@@ -0,0 +1,85 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const MONGODB_URI = `mongodb+srv://joycelapilus:${process.env.MONGODB_CLUSTER_PWD}@fullstack-lego.lyvzb.mongodb.net/?retryWrites=true&w=majority&appName=fullstack-lego`;
+const MONGODB_DB_NAME = "lego";
+
+const DEALS_COLLECTION = "deals";
+const SALES_COLLECTION = "sales";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(MONGODB_URI, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function insertDeals(db) {
+	// Read and parse the deals.json file
+	const dealsFilePath = "data/deals.json";
+	const deals = [...JSON.parse(fs.readFileSync(dealsFilePath, "utf-8"))];
+
+	const collection = db.collection(DEALS_COLLECTION);
+	const result = await collection.insertMany(deals);
+
+	console.log(`${result.insertedCount} deals records inserted`);
+}
+
+async function insertSales(db) {
+	// Path to the folder containing the JSON files
+	const salesFolderPath = "data/sales";
+
+	// Read all the files in the folder
+	const salesFiles = fs.readdirSync(salesFolderPath);
+
+	const sales = [];
+
+	// Iterate over each file, read and parse the JSON content
+	for (const file of salesFiles) {
+		const filePath = path.join(salesFolderPath, file);
+		const fileContent = fs.readFileSync(filePath, "utf-8");
+		const salesData = JSON.parse(fileContent);
+
+		// Ensure the parsed content is an array and add it to sales
+		if (Array.isArray(salesData)) {
+			sales.push(...salesData);
+		} else {
+			console.warn(`File ${file} did not contain an array of sales data`);
+		}
+	}
+
+	// Insert all sales data into the "sales" collection
+	const collection = db.collection(SALES_COLLECTION);
+	const result = await collection.insertMany(sales);
+
+	console.log(`${result.insertedCount} sales records inserted`);
+}
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+
+		// Access the database
+		const database = client.db(MONGODB_DB_NAME);
+
+		// Add data to collections
+		await insertDeals(database);
+		await insertSales(database);
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+
+run().catch(console.dir);
